218334e4b74acdef0e215fe8bca1b096b237f1b0
oOv <dawid101w@gmail.com>
2018-10-24 16:31:09 +0200

    edit product form template

--- src/app/edit-product-form/edit-product-form.component.ts
+++ src/app/edit-product-form/edit-product-form.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-product-form',
   templateUrl: './edit-product-form.component.html',
-  styleUrls: ['./edit-product-form.component.css']
+  styleUrls: ['./edit-product-form.component.scss']
 })
 export class EditProductFormComponent implements OnInit {
-  name = new FormControl('');
+  editProductForm = this.fb.group({
+    name: [''],
+    kcal: [''],
+    price: [''],
+    description: ['']
+  });
   
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
